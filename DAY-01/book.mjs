7b19be3529aefbcbf3107d52597e5260c83e2922
Mashhad Ali Shah <165510733+MASHHADALI@users.noreply.github.com>
2024-11-27 19:50:10 +0500

    Add files via upload

---
+++ DAY-01/book.mjs
@@ -0,0 +1,269 @@
+export{books};
+const books =[
+
+
+{
+
+    Id :1,
+    title :"The Catcher in The Rye",
+    author :"JD Salinger",
+    rating :4.5,
+    genre :"Fiction",
+},
+
+{
+
+    Id :2,
+    title :"To Kill a Mockingbird",
+    author :"Harper Lee",
+    rating :4.8,
+    genre :"Fiction",
+
+
+},
+
+{
+
+    Id :3,
+    title :"1984",
+    author :"George Orwell",
+    rating :4.7,
+    genre :"Fiction",
+},
+
+
+{
+
+    Id :4,
+    title :"Brave New World",
+    author :"Aldos Huxley",
+    rating :4.6,
+    genre :"Science",
+
+},
+
+{
+
+    Id :5,
+    title :"The Great Gatsby",
+    author :"F.Scott Fitzgerald",
+    rating :4.3,
+    genre :"Fiction",
+
+},
+
+{
+    Id :6,
+    title :"Pride and Prejudice",
+    author :"Jane Austin",
+    rating :4.9,
+    genre :"Romance",
+},
+
+{
+    Id :7,
+    title :"The Fault In Our Stars",
+    author :"John Green",
+    rating :4.8,
+    genre :"Romance",
+},
+
+{
+    Id :8,
+    title :"The Time Travelers Wife",
+    author :"Audrey Niffeneger",
+    rating :4.5,
+    genre :"Romance",
+
+},
+
+{
+    Id :9,
+    title :"A Brief History of time",
+    author :"Stephen Hawking",
+    rating :4.7,
+    genre :"Science",
+},
+
+{
+    Id :10,
+    title :"The Teory of everything",
+    author :"Stephen Hawking",
+    rating :4.6,
+    genre :"Science",
+
+},
+
+{
+    Id :11,
+    title :"The Hobbit",
+    author :"JRR Tolkien",
+    rating :4.7,
+    genre :"Fiction",
+},
+
+
+{
+    Id :12,
+    title :"The Lord Of the rings",
+    author :"JRR Tolkien",
+    rating :4.9,
+    genre :"Fiction",
+},
+
+{
+    Id :13,
+    title :"Wuthering Heights",
+    author :"Emily Bronte",
+    rating :4.6,
+    genre :"Romance",
+},
+
+{
+    Id :14,
+    title :"Dune",
+    author :"Frank Herbert",
+    rating :4.8,
+    genre :"Science",
+},
+
+{
+    Id :15,
+    title :"The Hunger Games",
+    author :"Suzane collins",
+    rating :4.5,
+    genre :"Fiction",
+},
+
+{
+
+    Id :16,
+    title :"The Alchemist",
+    author :"Paulo Coelho",
+    rating :4.8,
+    genre :"Fiction",
+},
+
+{
+    Id :17,
+    title :"The Note Books",
+    author :"Niholas Sparks",
+    rating :4.4,
+    genre :"Romance",
+},
+
+{
+    Id :18,
+    title :"Fiftey Shades OF Grey",
+    author :"EL James",
+    rating :4.2,
+    genre :"Romance",
+},
+
+
+{
+
+    Id :19,
+    title :"The Martian",
+    author :"Andy Weir",
+    rating :4.8,
+    genre :"Science",
+},
+
+{
+
+    Id :20,
+    title :"A wrinke in time",
+    author :"Madeline l Engle",
+    rating :4.6,
+    genre :"Science",
+},
+
+{
+
+    Id :21,
+    title :"The Picyure of dorean grey",
+    author :"Oscar Wild ",
+    rating :4.5,
+    genre :"Fiction",
+},
+
+{
+
+    Id :22,
+    title :"The Secret Garden",
+    author :"Frances Hodgson",
+    rating :4.7,
+    genre :"Fiction",
+},
+
+{
+    Id :23,
+    title :"Jane Eyre",
+    author :"Charlote Bronte",
+    rating :4.9,
+    genre :"Romance",
+},
+
+
+{
+    Id :24,
+    title :"The Help",
+    author :"Katherine Socket",
+    rating :4.9,
+    genre :"Fiction",
+},
+
+{
+
+    Id :25,
+    title :"The Fault In our stars",
+    author :"John Green",
+    rating :4.8,
+    genre :"Romance",
+},
+
+
+{
+    Id :26,
+    title :"The Road",
+    author :"JRR Tolkien",
+    rating :4.2,
+    genre :"Fiction",
+},
+
+{
+Id :27,
+    title :"The Science Of Intersteller",
+    author :" Kip Throne ",
+    rating :4.6,
+    genre :"Science",
+},
+
+
+{
+
+    Id :28,
+    title :"Atlas Shrugged",
+    author :"Ayn Rand",
+    rating :4.3,
+    genre :"Fiction",
+},
+
+{
+    Id :29,
+    title :"Enders Game",
+    author :"Orson Scott",
+    rating :4.7,
+    genre :"Science",
+},
+
+{
+    Id :30,
+    title :"The Rosie Project",
+    author :"Graaene simpson",
+    rating :4.6,
+    genre :"Romance",
+  },
+ 
+];
---
+++ DAY-01/manager.mjs
@@ -0,0 +1,36 @@
+import {books} from "./book.mjs";
+// console.log(books);
+// const booksOfhigherRating = () =>{
+//        return books.filter( b=> b.rating > 4.5)
+    
+//     }
+    
+    
+//     const booksRATING = booksOfhigherRating()
+//     console.log(booksRATING)
+    
+    
+    // FUNCTION 2.
+    
+    // const printBookDetails = () => {
+    //     books.forEach(({ title, author, rating, genre }) => {
+    //         console.log(`Title: ${title}, Author: ${author}, Rating: ${rating}, Genre: ${genre}`);
+    //     });
+    // };
+    // printBookDetails();
+    
+    
+    // FICTION books
+    
+    // const fictionBooks = books.filter(books => books.genre === "Fiction")
+    // console.log("Fiction Books:", fictionBooks);
+    
+
+    // const fictionbook= books.filter(books => books.genre=="Fiction")
+        
+    
+    
+    // DISPLAYING BOOK AUTHORS
+    
+    // const bookAuthors = books.map(book => book.author);
+    // console.log("Authors:", bookAuthors);
